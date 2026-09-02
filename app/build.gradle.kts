a304cb7438fe64da394acb2239530609e760de07
greenpower2669 <80179295+greenpower2669@users.noreply.github.com>
2026-09-02 19:19:23 +0200

    Bump FabData to v0.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.fabdata.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.10.0"
+        versionCode = 20
+        versionName = "0.10.1"
     }
 
     signingConfigs {
